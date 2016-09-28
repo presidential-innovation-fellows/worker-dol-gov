c914d1c65bae5d0c359ac534b1b8b5dc03dfc957
adamsfallen <adam.bonnifield@gmail.com>
2016-09-28 11:58:41 -0400

    Add Autocomplete for Homepage
    Awesomplete for homepage.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,10 @@ $(function() {
     $("a.learn-rights").attr("href", params["selected"])
   });
 
-  $('input.index-input').on('input propertychange paste', function() {
-  		var url = $("#workerprofiles option[value='" + $('input.index-input').val() + "']").attr('data-url');
-        if(url !== undefined && url !== null) { 
-        	$("a.learn-rights").attr("href", url)
-        }	
-        else { 
-    		$("a.learn-rights").attr("href", "profiles/all.html")
-		}
-  });
-
   var comboplete = new Awesomplete('input.dropdown-input-index', {
   minChars: 0,
   });
+
   
   Awesomplete.$('.dropdown-btn').addEventListener("click", function() {
   if (comboplete.ul.childNodes.length === 0) {
@@ -34,31 +25,26 @@ $(function() {
   else {
     comboplete.close();
   }
-});
-
-
-
-
-  var comboplete = new Awesomplete('input.dropdown-input', {
-  minChars: 0,
   });
-  
-  Awesomplete.$('.dropdown-btn').addEventListener("click", function() {
-  if (comboplete.ul.childNodes.length === 0) {
-    comboplete.minChars = 0;
-    comboplete.evaluate();
-  }
-  else if (comboplete.ul.hasAttribute('hidden')) {
-    comboplete.open();
-  }
-  else {
-    comboplete.close();
-  }
-});
 
-  document.querySelector('input.dropdown-input').addEventListener('awesomplete-selectcomplete', function(evt){
-    var url = $("#mylist option[value='" + $('input.dropdown-input').val() + "']").attr('data-url');
-    window.location.href = url;
-  })
+  Awesomplete.$('input.dropdown-input-index').addEventListener("awesomplete-selectcomplete", function() {
+      updateLearnMore();
+  });
+
+  $('input.dropdown-input-index').on('input propertychange paste', function() {
+      updateLearnMore();
+  });
+
+
+  function updateLearnMore() {
+   var url = $("#workerprofiles option[value='" + $('input.dropdown-input-index').val() + "']").attr('data-url');
+        if(url !== undefined && url !== null) { 
+          $("a.learn-rights").attr("href", url)
+        } 
+        else { 
+        $("a.learn-rights").attr("href", "profiles/all.html")
+    }
+ }
+
 
 });
