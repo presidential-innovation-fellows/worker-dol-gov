3b90db72858c0621f3e2369988236513360fbd86
Noah Manger <noah.manger@gsa.gov>
2017-08-01 16:57:30 -0700

    Fix concern selection bug

--- assets/js/concerns.js
+++ assets/js/concerns.js
@@ -1,34 +1,13 @@
 // Find all concerns and details
-var concerns = document.getElementsByClassName('js-concern');
+var concerns = document.getElementsByClassName('js-concern-button');
 var details = document.getElementsByClassName('js-concern-details');
 
-// Takes a slug ID like i-was-discriminated-against and highlights the button and shows the details
-function selectConcern(id) {
-  // Deselect all concerns
-  [].forEach.call(concerns, function(concern) {
-    concern.setAttribute('aria-selected', 'false');
-  });
-
-  // Show all details
-  [].forEach.call(details, function(div) {
-    div.setAttribute('hidden', '');
-  });
-
-  // Select the concern that matches the ID and show the details group that matches the ID
-  document.querySelector('[aria-controls="'+ id +'"]').setAttribute('aria-selected', 'true');
-  document.getElementById(id).removeAttribute('hidden');
-}
-
-// If a hash was passed, go highlight the concerns
+// If a hash was passed, go open the right concern
 if (window.location.hash) {
   var hash = window.location.hash.split('#')[1];
-  selectConcern(hash);
-}
+  var id = 'accordion-' + hash;
 
-// Attach event listeners to all concerns
-for (var i = 0; i < concerns.length; i++) {
-  concerns[i].addEventListener('click', function(e) {
-    var id = e.target.getAttribute('aria-controls');
-    selectConcern(id);
-  });
+  // Select the concern that matches the ID and show the details group that matches the ID
+  document.querySelector('[aria-controls="'+ id +'"]').setAttribute('aria-expanded', 'true');
+  document.getElementById(id).removeAttribute('hidden');
 }
